609406b54f9532f11edde38ae51bb07b463e1281
kamal ibrahim <ibrahimkamal314@gmail.com>
2026-09-21 02:53:24 +0300

    fix vercel serverless api routing1

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
-import app, { initApp } from "./src/app.controller.js";
+import app, { initApp } from "../src/app.controller.js";
 
-// Ensure Express application middleware, routes, and error handlers are initialized
 initApp();
 
-// Export the Express app instance for Vercel Serverless Function execution
 export default app;
